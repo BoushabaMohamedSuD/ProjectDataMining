434cab262e304506389b4c1585617ac083984ef0
Boushaba Mohamed <med1998yz@gmail.com>
2020-03-22 18:32:40 +0100

    Convert Columns (join-date & quit-date) on date time

--- Project.py
+++ Project.py
@@ -7,13 +7,23 @@ import pandas as pd
 #reading dataset
 
 dataset=pd.read_csv('employee_retention_data.csv')
-#df=pd.DataFrame(dataset)
-
 
 #changing the columns dataset on lower case
 
 dataset.columns= map(str.lower, dataset.columns)
 
 #Check column data types
+print('#######Check column data types##########')
 datasetTypeSeries=dataset.dtypes
 print(datasetTypeSeries)
+
+
+#Convert Columns (join-date & quit-date) on date time
+
+dataset['join_date']= pd.to_datetime(dataset['join_date'])
+dataset['quit_date']= pd.to_datetime(dataset['quit_date'])
+print('#######Check column data types##########')
+datasetTypeSeries=dataset.dtypes
+print(datasetTypeSeries)
+
+
