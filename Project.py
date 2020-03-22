7cf77e53b7e3a7f770053edbd536d68d2cb45db5
Boushaba Mohamed <med1998yz@gmail.com>
2020-03-22 18:06:03 +0100

    adding librares and dataset

--- Project.py
+++ Project.py
@@ -1,2 +1,9 @@
-x=6;
-print(x);
+# importing libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#reading dataset
+
+dataset=pd.read_csv('employee_retention_data.csv')
