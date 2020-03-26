4b177851eae9e79a4d5287b6efcf7cfdf7bb25a5
Boushaba Mohamed <med1998yz@gmail.com>
2020-03-26 19:29:12 +0100

    partie 3 templet

---
+++ Partie3.py
@@ -0,0 +1,14 @@
+######
+# Prétraitement de données
+#####
+# importing libraries
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+import datetime as dtime
+
+#reading dataset
+
+dataset=pd.read_csv('export/datasetClean.csv')
